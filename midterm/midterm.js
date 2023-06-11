23d62912047fa4a3f9b6540def8fb3182a6bc1c1
TheBestWebDeveloper <130028018+TheBestWebDeveloper@users.noreply.github.com>
2023-06-10 22:14:16 -0700

    Update midterm.js

--- midterm/midterm.js
+++ midterm/midterm.js
@@ -1,19 +1,22 @@
 function validateForm() {
-var name = document.getElementById("name").value;
-var email = document.getElementById("email").value;
-var comments = document.getElementById("comments").value;
-var letters = /^[A-Za-z]+$/;
-if (name == "" || email == "" || comments == "") {
-alert("Please fill in all fields.");
-return false;
-}
-if (!name.match(letters)) {
-alert("Please enter letters only in the name field.");
-return false;
-}
-if (email.indexOf("@") == -1) {
-alert("Please enter a valid email address.");
-return false;
-}
-return true;
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var comments = document.getElementById("comments").value;
+  var letters = /^[A-Za-z]+$/;
+  var errorDiv = document.getElementById("errorDiv");
+  errorDiv.innerHTML = "";
+  var isValid = true;
+  if (name == "" || email == "" || comments == "") {
+      errorDiv.innerHTML += "Please fill in all fields.<br>";
+      isValid = false;
+  }
+  if (!name.match(letters)) {
+      errorDiv.innerHTML += "Please enter letters only in the name field.<br>";
+      isValid = false;
+  }
+  if (email.indexOf("@") == -1) {
+      errorDiv.innerHTML += "Please enter a valid email address.<br>";
+      isValid = false;
+  }
+  return isValid;
 }
